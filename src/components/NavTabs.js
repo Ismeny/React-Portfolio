f431952e1dd79c5e701b2097ab6f27810b4ebfbb
Ismeny Saguilan <menysag2@gmail.com>
2022-01-07 09:37:43 -0600

    updated navtab name to projects

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -30,7 +30,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
         >
-          Projectss
+          Projects
         </a>
         <a href="#resume"  
           onClick={() => handlePageChange('Resume')}
