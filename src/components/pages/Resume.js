15504f97f32866f7ec7e9ec838ba9c7ebb948368
Ismeny Saguilan <menysag2@gmail.com>
2022-02-10 16:20:39 -0600

    added resume skills, button, color changes

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,7 +8,33 @@ export default function Resume() {
     <>
     <div className='resume'>
       <h1 className='h1'>Resume</h1>
-      <a className= 'resume-link'href={resume} target="_blank" rel="noreferrer"> Click to view resume</a>
+      <div className='resume-skills'>
+        <h3>Front-end Proficiencies</h3>
+        <ul className="skills">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Responsive Design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ul>
+        <h3>Back-end Proficiencies</h3>
+        <ul className="skills">
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL</li>
+          <li>Sequelize</li>
+          <li>MongoDB</li>
+          <li>Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
+        </div>
+        <button>
+    <a className= 'resume-link'href={resume} target="_blank" rel="noreferrer"> Download Resume</a>
+    </button>
     </div>
     <footer className='footer'>
       <h5 className='center-text'> Copyright &copy; 2021 Ismeny Saguilan</h5>
